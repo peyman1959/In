c47b598ddeb05989981730668ea50c56a99bee20
Peyman Ansari <peyman1958@yahoo.com>
2020-01-31 16:10:57 +0330

    first

---
+++ In/Assets/Script/Pipe.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pipe : MonoBehaviour
+{
+    public List<Rigidbody> rb;
+    public Vector3 dir;
+    public float power;
+    private void OnMouseDown()
+    {
+        foreach (Rigidbody rigidbody in rb)
+        {
+            rigidbody.AddForce(dir*power);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        
+        rb.Add(other.GetComponent<Rigidbody>());
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        rb.Remove(other.GetComponent<Rigidbody>());
+    }
+}
